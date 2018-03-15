b69ff9fc7e5edf454b6221366d5aa7099cf08339
Diarmid Flatley <diarmid@169-231-138-202.wireless.ucsb.edu>
2018-03-15 12:56:41 -0700

    integrate Karl's Allosphere edits

--- final/common.hpp
+++ final/common.hpp
@@ -1,19 +1,21 @@
 #ifndef __COMMON__
 #define __COMMON__
 
-#include "allocore/io/al_App.hpp"
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include "Cuttlebone/Cuttlebone.hpp"
+#include "allocore/al_Allocore.hpp"
 using namespace al;
 
 // Common definition of application state
 //
 struct State {
-
-//  Vec3f vertex[14];
+  //  Vec3f vertex[14];
   Vec3f cursorPosition;
   Vec3f navPosition;
   Quatd navOrientation;
-//  vector<int> connections[14];
-
+  //  vector<int> connections[14];
 };
 
 #endif
